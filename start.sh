fe0b77790d4821b8923dda23fc1004c4e41666d7
Aysu <aysuyzdctn@gmail.com>
2026-02-18 23:07:31 +0300

    failway

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 PORT="${PORT:-8000}"
-celery -A catalog_worker worker --loglevel=info --concurrency=2 &
+celery -A celery_app.celery_app worker --loglevel=info --concurrency=2 &
 exec uvicorn api:app --host 0.0.0.0 --port "$PORT"
